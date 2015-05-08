70b0a22a30abf44a297601f5449651f7f40662ba
brisebois <brisebois@outlook.com>
2015-05-08 09:21:56 -0400

    Initial Commit

---
+++ DSC/DataDisk.ps1
@@ -0,0 +1,18 @@
+﻿configuration DataDisk {
+    param (
+        [UInt64]$DriveSize = 32TB,
+        [UInt32]$NumberOfColumns = 32
+             
+    )
+
+    Import-DscResource -module cDiskTools
+    node localhost
+    {   
+        cDiskTools StripeVHDsAndPrepareVolume 
+        {
+            DriveSize = $DriveSize
+            NumberOfColumns = $NumberOfColumns
+        }
+    }
+}
+DataDisk
---
+++ DSC/MakeDSC.ps1
@@ -0,0 +1,11 @@
+﻿New-xDscResource –Name cStripeVHDsAndPrepareVolume `
+                 -Property (New-xDscResourceProperty –Name DriveSize `
+                                                     –Type UInt64 `
+                                                     -Attribute Key), `
+                           (New-xDscResourceProperty –Name NumberOfColumns `
+                                                     -Type Uint32 `
+                                                     -Attribute Required) `
+                 -Path 'C:\Program Files\WindowsPowerShell\Modules\' `
+                 -ModuleName cDiskTools `
+                 -FriendlyName cDiskTools `
+                 -Verbose
